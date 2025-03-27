210b4f673f61a06f34667de01f7233e0dca179ce
muskan514 <150267291+muskan514@users.noreply.github.com>
2025-03-27 23:37:50 +0530

    commit4

---
+++ mvj_java/src/com/Question1/Demo1.java
@@ -0,0 +1,16 @@
+package com.Question1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+        Student1 student = new Student1();
+
+        // Creating Commission object and using its methods
+        Commission employee = new Commission();
+        employee.acceptDetails();
+        employee.calculateCommission();
+
+	}
+
+}
---
+++ mvj_java/src/com/Question2/Demo2.java
@@ -0,0 +1,16 @@
+package com.Question2;
+
+public class Demo2 {
+	public static void main(String[] args) {
+	       
+        Student2 student = new Student2();
+       
+
+        Commission employee = new Commission();
+        employee.acceptDetails();
+        employee.calculateCommission();
+        Student2 circle = new Student2();
+        circle.getInput();
+        circle.calcArea();
+
+}}
